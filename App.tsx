281e051bab21ed1b05e32eeaa310f36782284604
Muhammad Saleh <m7md.sal762@gmail.com>
2022-07-10 11:03:54 +0200

    remove bottom nav

--- App.tsx
+++ App.tsx
@@ -9,25 +9,7 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MapScreen from './App/Components/Screens/MapScreen';
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <>
-      <MapScreen />
-      <SafeAreaView>
-      </SafeAreaView>
-    </>
-  );
-};
+const App = () => <MapScreen />;
 
 export default App;
